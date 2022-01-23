a25a5566c283d7eb495172c1d74a6b5cd8a77875
Manzaraz <magnusmanz@gmail.com>
2022-01-22 21:22:22 -0300

    finished

--- server.js
+++ server.js
@@ -2,32 +2,45 @@
 // where your node app starts
 
 // init project
-require('dotenv').config();
-var express = require('express');
+require("dotenv").config();
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
-
+app.get("/api/whoami", function (req, res) {
+  let software = req.headers["user-agent"].split(" "),
+    data = {
+      ipaddress: req.headers["x-forwarded-for"],
+      language: req.headers["accept-language"].split(",")[0],
+      software:
+        software[1].slice(1) +
+        " " +
+        software[2] +
+        " " +
+        software[3].slice(0, -1),
+    };
+  console.log(data);
+  res.json(data);
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
